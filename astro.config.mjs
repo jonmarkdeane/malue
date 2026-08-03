48ae22e5e44b375d6f946a72dfc3c8ddd0e714d0
Jon Mark Deane <jon@jonmarkdeane.com>
2026-08-03 17:01:17 +0100

    fffff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jonmarkdeane.github.io',
-  base: '/',
-    fonts: [{
-    provider: fontProviders.fontsource(),
-    name: "Inter",
-    cssVariable: "--font-inter",
-  }]
-}) 
+  site: "https://jonmarkdeane.github.io",
+  base: "/malue",
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    },
+  ],
+});
