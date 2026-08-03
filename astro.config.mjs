ab6ceed32b3631d5a11e30d69c7a58c3027c50b0
Jon Mark Deane <jon@jonmarkdeane.com>
2026-08-03 15:57:33 +0100

    now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jonmarkdeane.github.io',
+  base: '/malue',
     build: {
     assetsPrefix: './',
   },
